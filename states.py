de127c7fbce228c264c411105ef2569235fbc2ca
Kirtley.Kevin.1143572699 <kevin.w.kirtley.ctr@nga.mil>
2021-06-13 14:49:49 -0500

    Finalized bonus work and other files.

--- states.py
+++ states.py
@@ -5,14 +5,14 @@ states = pd.read_csv('./States.csv')
 
 pd.set_option('precision', 3)
 
-states.columns = ['State', 'Region', 'Population',
-                  'SATV', 'SATM', 'Percent', 'Dollars', 'Pay']
+states.columns = ['State', 'Region', 'Population', 'SATV', 'SATM', 'Percent', 'Dollars', 'Pay']
+
 
 # reflect the actual population by multiplying 1000
 states.Population = states['Population'] * 1000
 # reflect actual thousands spent on each student
 states.Dollars = states['Dollars'] * 1000
-print(states[['State', 'Population', 'Dollars']], '\n')
+print(states[ ['State', 'Population', 'Dollars'] ], '\n') 
 
 print(states.describe(), '\n')
 
